1bc9c856723f510711fe5b7167ddf895c7588f07
Your Name <you@example.com>
2020-03-23 19:01:17 +0430

    added timer & false llist

--- src/App.js
+++ src/App.js
@@ -1,20 +1,62 @@
-import React,{useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [items, setItems] = useState(0);
+  const [isActive, setIsActive] = useState(false);
 
-const toggelHandler =()=>{
-  console.log('hi')
-}
-const stopHandler =()=>{
-  console.log('bye')
-}
- return <>
- <button
- onClick={toggelHandler} >toggel</button>
- <button
- onClick={stopHandler}>stop</button>
- </>
+  const toggel = () => {
+    setIsActive(!isActive);
+  }
+
+  const stop = () => {
+    setCount(0);
+    setIsActive(false);
+  }
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setCount(count => count + 1);
+        setItems(items.map((item,selectedIndex)=>renderItem(item, selectedIndex)));
+      }, 1000);
+    } else if (!isActive && count !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, count , items]);
+
+  const renderItem = (item, selectedIndex) => {
+    return [
+      1 + selectedIndex,
+      <>
+        <li>{item}</li>
+      </>
+    ]
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log('This will run every second!');
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return <>
+    <div>
+      {count}
+      {/* {renderItem()} */}
+    </div>
+
+    <button className={`button button-primary button-primary-${isActive ? 'active' : 'inactive'}`}
+      onClick={toggel} >
+      {isActive ? 'revers' : 'toggel'}
+    </button>
+    <button
+      onClick={stop}>stop</button>
+  </>
 }
 
 export default App;
