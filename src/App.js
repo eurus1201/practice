17f38ca1d9b62d717b293a5b95cd7ea765a6deeb
Your Name <you@example.com>
2020-03-24 19:06:40 +0430

    added push for add nums to end of the list

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState([[0]]);
   const [isActive, setIsActive] = useState(false);
 
   const toggel = () => {
     setIsActive(!isActive);
   }
-  // const count= [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 
   const renderItem = (item) => {
     return [
@@ -20,7 +19,7 @@ function App() {
 
 
   const stop = () => {
-    setCount(0);
+    setCount([[0]]);
     setIsActive(!isActive);
   }
 
@@ -38,13 +37,11 @@ function App() {
 
   console.log(count)
 
-  const list = !!count && setInterval(()=>{
-    count.map(renderItem);
-  },1000);
+  const list = !!count&& count.push(renderItem);
+  
 
   return <>
     <div>
-      {count}s
       {list}
     </div>
 
