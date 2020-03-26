7c22d9ae64d978d334be8b5e799155c2d488bdd4
Your Name <you@example.com>
2020-03-26 21:28:44 +0430

    some changes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,50 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
 
 function App() {
-  const [items, setItems] = useState([0]);
-  const [active, setActive] = useState(false);
+  const [items, setItems] = useState([]);
+  const [add, setAdd] = useState(true);
+  const [counter, setCounter] = useState(0);
 
-  const toggel=()=>{
-    setActive(!active)
+  const toggel = () => {
+    setAdd(!add)
   }
 
-  useEffect(()=>{
+  const count = () => {
+    let i=0
+    if (add) {
+        setCounter(i++);
+    } else if (!add) {
+        setCounter(--i);
+    }
+  }
+  useEffect(() => {
+    count()
+  }, [])
+
+  useEffect(() => {
     let interval = null;
-    if(active){
+    if (add) {
       interval = setInterval(() => {
-        setItems(items => [...items, items++])
+        setItems(items => [...items,`${counter}`])
       }, 1000);
-    }else if(!active) {
-      clearInterval(interval);
+    } else if (!add && items!== 0) {
+      interval = setInterval(() => {
+        setItems(items => [`${counter}`, ...items])
+      }, 1000);
     }
     return () => clearInterval(interval);
-  },[active,items])
+  }, [add])
 
   const stop = () => {
-    setActive(active);
-    setItems([0]);
+    setAdd(false);
+    setItems([0])
   }
 
   return (
     <div>
-    <div>
-    {items.map((items,i)=><li key={i}>{items}<input></input></li>)}
-    </div>
+      <div>
+        {items.map((item, i) => <li key={i}>{item}<input></input></li>)}
+      </div>
       <button onClick={toggel} >
         toggel
       </button>
