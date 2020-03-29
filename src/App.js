5090ba938e5cd08a6c663fa2a19ff4db31366cbf
Your Name <you@example.com>
2020-03-29 12:27:19 +0430

    started over

--- src/App.js
+++ src/App.js
@@ -1,66 +1,36 @@
-import React, { useEffect, useState,useRef } from 'react';
+import React, { useState, useEffect, useRef } from "react";
 
 function App() {
-  const [items, setItems] = useState([]);
-  const [add, setAdd] = useState(true);
-  const [reverse, setReverse] = useState(false);
-  let [counter, setCounter] = useState([]);
-  const intervalRef = useRef();
+    const [count, setCount] = useState([0]);
+    const [item, setItem] = useState([]);
+    const intervalRef = useRef();
 
-  const toggel = () => {
-    setAdd(!add);
-    setReverse(!reverse)
-  }
 
-  const count = () => setCounter(counter++);
+    useEffect(() => {
+        const id = setInterval(() => {
+            setCount(count => [+ count + 1])
+            setItem(item => [...item, `${count}`])
+        }, 1000);
+        intervalRef.current = id;
+        return () => clearInterval(intervalRef.current);
+    }, [count]);
+    const handleCancel = () => clearInterval(intervalRef.current);
 
-  // useEffect(()=>{
-  //   count()
-  // },[])
-
-  // useEffect(() => {
-  //   if (add) {
-  //     counter = counter+1;
-  //   }else if (!add){
-  //     counter = counter-1;
-  //   }
-  // }, [])
-
-  useEffect(() => {
-    count();
-    let interval = null;
-    if (add) {
-      interval = setInterval(() => {
-        setItems(items => [...items,`${counter++}`])
-      }, 1000);
-    } else if (reverse &&items!== 0) {
-      interval = setInterval(() => {
-        setItems(items => [`${counter--}`,...items])
-      }, 1000);
-    }
-    intervalRef.current = interval;
-    return () => clearInterval(intervalRef.current);
-  }, [add,reverse])
-
-  const stop = () => {
-    setAdd(!add) ;
-    setReverse(false);
-    clearInterval(intervalRef.current);
+    return (
+        <>
+            <div>
+                {item.map((item, i) => <li key={i}>{item}<input></input></li>)}
+            </div>
+            <div>
+                <button>
+                    toggel
+                </button>
+                <button
+                onClick={handleCancel}>
+                    stop
+                </button>
+            </div>
+        </>
+    );
 }
-
-  return (
-    <div>
-      <div>
-        {items.map((item, i) => <li key={i}>{item}<input></input></li>)}
-      </div>
-      <button onClick={toggel} >
-        {reverse ? 'revers' : 'toggel'}
-      </button>
-      <button onClick={stop}>
-        {add || reverse ?'stop':'start'}
-        </button>
-    </div>
-  )
-}
-
 export default App;
